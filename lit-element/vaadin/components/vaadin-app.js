2ed06794194c34e40405ebea93c199b9905e5c3f
web-padawan <iamkulykov@gmail.com>
2020-02-17 10:58:43 +0200

    Add vaadin-app-layout to benchmarks

--- lit-element/vaadin/components/vaadin-app.js
+++ lit-element/vaadin/components/vaadin-app.js
@@ -1,5 +1,7 @@
 import { LitElement, html, css } from 'lit-element';
 import '@vaadin/vaadin-accordion/vaadin-accordion.js';
+import '@vaadin/vaadin-app-layout/vaadin-app-layout.js';
+import '@vaadin/vaadin-app-layout/vaadin-drawer-toggle.js';
 import '@vaadin/vaadin-details/vaadin-details.js';
 import '@vaadin/vaadin-tabs/vaadin-tabs.js';
 
@@ -15,27 +17,30 @@ class VaadinApp extends LitElement {
 
   render() {
     return html`
-      <vaadin-tabs>
-        <vaadin-tab>Tab 1</vaadin-tab>
-        <vaadin-tab>Tab 2</vaadin-tab>
-        <vaadin-tab>Tab 3</vaadin-tab>
-      </vaadin-tabs>
+      <vaadin-app-layout>
+        <vaadin-drawer-toggle slot="navbar touch-optimized"></vaadin-drawer-toggle>
+        <h3 slot="navbar touch-optimized">Vaadin app</h3>
+        <vaadin-tabs orientation="vertical" slot="drawer">
+          <vaadin-tab>Tab 1</vaadin-tab>
+          <vaadin-tab>Tab 2</vaadin-tab>
+          <vaadin-tab>Tab 3</vaadin-tab>
+        </vaadin-tabs>
+        <vaadin-details opened>
+          <div slot="summary">Details</div>
+          Details content
+        </vaadin-details>
 
-      <vaadin-details opened>
-        <div slot="summary">Details</div>
-        Details content
-      </vaadin-details>
-
-      <vaadin-accordion>
-        <vaadin-accordion-panel theme="filled">
-          <div slot="summary">Accordion Panel 1</div>
-          <div>Panel content</div>
-        </vaadin-accordion-panel>
-        <vaadin-accordion-panel theme="filled">
-          <div slot="summary">Accordion Panel 2</div>
-          <div>Panel content</div>
-        </vaadin-accordion-panel>
-      </vaadin-accordion>
+        <vaadin-accordion>
+          <vaadin-accordion-panel theme="filled">
+            <div slot="summary">Accordion Panel 1</div>
+            <div>Panel content</div>
+          </vaadin-accordion-panel>
+          <vaadin-accordion-panel theme="filled">
+            <div slot="summary">Accordion Panel 2</div>
+            <div>Panel content</div>
+          </vaadin-accordion-panel>
+        </vaadin-accordion>
+      </vaadin-app-layout>
     `;
   }
 }
--- polymer/vaadin/components/vaadin-app.js
+++ polymer/vaadin/components/vaadin-app.js
@@ -1,5 +1,7 @@
 import { PolymerElement, html } from '@polymer/polymer';
 import '@vaadin/vaadin-accordion/vaadin-accordion.js';
+import '@vaadin/vaadin-app-layout/vaadin-app-layout.js';
+import '@vaadin/vaadin-app-layout/vaadin-drawer-toggle.js';
 import '@vaadin/vaadin-details/vaadin-details.js';
 import '@vaadin/vaadin-tabs/vaadin-tabs.js';
 
@@ -14,27 +16,30 @@ class VaadinApp extends PolymerElement {
         }
       </style>
 
-      <vaadin-tabs>
-        <vaadin-tab>Tab 1</vaadin-tab>
-        <vaadin-tab>Tab 2</vaadin-tab>
-        <vaadin-tab>Tab 3</vaadin-tab>
-      </vaadin-tabs>
+      <vaadin-app-layout>
+        <vaadin-drawer-toggle slot="navbar touch-optimized"></vaadin-drawer-toggle>
+        <h3 slot="navbar touch-optimized">Vaadin app</h3>
+        <vaadin-tabs orientation="vertical" slot="drawer">
+          <vaadin-tab>Tab 1</vaadin-tab>
+          <vaadin-tab>Tab 2</vaadin-tab>
+          <vaadin-tab>Tab 3</vaadin-tab>
+        </vaadin-tabs>
+        <vaadin-details opened>
+          <div slot="summary">Details</div>
+          Details content
+        </vaadin-details>
 
-      <vaadin-details opened>
-        <div slot="summary">Details</div>
-        Details content
-      </vaadin-details>
-
-      <vaadin-accordion>
-        <vaadin-accordion-panel theme="filled">
-          <div slot="summary">Accordion Panel 1</div>
-          <div>Panel content</div>
-        </vaadin-accordion-panel>
-        <vaadin-accordion-panel theme="filled">
-          <div slot="summary">Accordion Panel 2</div>
-          <div>Panel content</div>
-        </vaadin-accordion-panel>
-      </vaadin-accordion>
+        <vaadin-accordion>
+          <vaadin-accordion-panel theme="filled">
+            <div slot="summary">Accordion Panel 1</div>
+            <div>Panel content</div>
+          </vaadin-accordion-panel>
+          <vaadin-accordion-panel theme="filled">
+            <div slot="summary">Accordion Panel 2</div>
+            <div>Panel content</div>
+          </vaadin-accordion-panel>
+        </vaadin-accordion>
+      </vaadin-app-layout>
     `;
   }
 }

74237700a7fb0bed1ad91bee06539829fc0b6191
web-padawan <iamkulykov@gmail.com>
2020-04-20 10:20:26 +0300

    Add vaadin-progress-bar to benchmarks

--- lit-element/vaadin/components/vaadin-app.js
+++ lit-element/vaadin/components/vaadin-app.js
@@ -3,6 +3,7 @@ import '@vaadin/vaadin-accordion/vaadin-accordion.js';
 import '@vaadin/vaadin-app-layout/vaadin-app-layout.js';
 import '@vaadin/vaadin-app-layout/vaadin-drawer-toggle.js';
 import '@vaadin/vaadin-details/vaadin-details.js';
+import '@vaadin/vaadin-progress-bar/vaadin-progress-bar.js';
 import '@vaadin/vaadin-radio-button/vaadin-radio-group.js';
 import '@vaadin/vaadin-tabs/vaadin-tabs.js';
 
@@ -47,6 +48,8 @@ class VaadinApp extends LitElement {
           <vaadin-radio-button value="1">Option 2</vaadin-radio-button>
           <vaadin-radio-button value="1">Option 3</vaadin-radio-button>
         </vaadin-radio-group>
+
+        <vaadin-progress-bar></vaadin-progress-bar>
       </vaadin-app-layout>
     `;
   }
